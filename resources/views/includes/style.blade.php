6aa61b3cedac8b0e52694cd771e23d5ea389b521
ZaidanNur <zaixyz332@gmail.com>
2026-02-26 19:48:35 +0700

    fixing error styling

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,5 +1,5 @@
 <!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+@vite(['resources/css/app.css'])
 <link href="{{ url("css/main.css") }}" rel="stylesheet">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
